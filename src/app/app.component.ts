95d04eade10348f6ee23dc3b34d655184d9a250c
Priestch <priestch@gmail.com>
2023-04-18 07:58:46 +0800

    setup foundation tabs demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import foundation from 'foundation-sites'
+import $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myApp';
+
+  @ViewChild('fbTabs')
+  private fbTabs: any;
+
+  ngAfterViewInit() {
+    new foundation.Tabs($(this.fbTabs.nativeElement));
+  }
 }
